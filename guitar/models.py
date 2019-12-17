cac24cd490ba6a8e18cc7cd1168471c849919c03
Abhijit Shete <abhijit.shete@happiestminds.com>
2019-12-17 10:34:38 +0530

    new data

--- guitar/models.py
+++ guitar/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-class chords(models.Model):
-    song_names = models.TextField(blank=True)
-    sargam = models.TextField(blank=True)
-    status = models.CharField(max_length=110)
-    url = models.CharField(max_length=300,default="old")
-   
 
 
 class chord(models.Model):
--- music/models.py
+++ music/models.py
@@ -2,19 +2,6 @@ from django.db import models
 
 
 
-class Songs(models.Model):
-    song_name = models.CharField(max_length=100,blank=True)
-    rating = models.CharField(max_length=100)
-    year = models.CharField(max_length=100)
-    singer = models.CharField(max_length=111)
-    notation = models.TextField(blank=True)
-    link = models.TextField(blank=True)
-    
-    
-
-class Sargam(models.Model):
-    song_names = models.TextField(blank=True)
-    sargam = models.TextField(blank=True)
    
     
     
